a7f2da96bc16d3b5bad1b82db8d5eee510456874
ricardohenrique <ricardo.hb@outlook.com>
2019-03-18 04:16:47 -0300

    init controller statitics

---
+++ app/Http/Controllers/StatisticsController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Services\StatisticsService;
+
+class StatisticsController extends Controller
+{
+    public $service;
+
+    /**
+     * Constructor class
+     * @access public
+     * @param StatisticsService $service
+     * @return void
+     */
+    public function __construct(StatisticsService $service)
+    {
+        $this->service = $service;
+    }
+
+    public function getMaxTemperature()
+    {
+        return response()->json($this->service->getMaxTemperature());
+    }
+
+    public function getMinTemperature()
+    {
+        return response()->json($this->service->getMinTemperature());
+    }
+
+    public function getMeanTemperature()
+    {
+        return response()->json($this->service->getMeanTemperature());
+    }
+
+    public function getObservations()
+    {
+        return response()->json($this->service->getObservations());
+    }
+
+    public function getDistance()
+    {
+        return response()->json($this->service->getDistance());
+    }
+}
